57ee82bacfa7c2f21e8445d02c4ed97657a97046
Wiktor Pytlewski <01149938@pw.edu.pl>
2023-01-20 18:21:09 +0100

    Delete build files

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/michalkopec/SEM5/ZPR_test/naprawa/tests
-# Build directory: /home/michalkopec/SEM5/ZPR_test/naprawa/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AgarioTest "_unit_tests")
-set_tests_properties(AgarioTest PROPERTIES  _BACKTRACE_TRIPLES "/home/michalkopec/SEM5/ZPR_test/naprawa/tests/CMakeLists.txt;30;add_test;/home/michalkopec/SEM5/ZPR_test/naprawa/tests/CMakeLists.txt;0;")
