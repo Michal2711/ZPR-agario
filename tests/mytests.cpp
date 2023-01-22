48947d2335ba02122c5c95f2f9d7405f38073966
Michał Kopeć <01159326@pw.edu.pl>
2023-01-22 16:22:51 +0100

    before pull

--- tests/mytests.cpp
+++ tests/mytests.cpp
@@ -1,9 +1,22 @@
 #include <gtest/gtest.h>
 #include "myfunctions.h"
+#include "../include/Game.h"
 
 TEST(myfunctions, add)
 {
     GTEST_ASSERT_EQ(add(10, 22), 32);
+}
+
+TEST(abc, abcde)
+{
+    GTEST_ASSERT_EQ(1, 1);
+}
+
+TEST(create_game, game)
+{
+    Game game;
+    
+
 }
 
 int main(int argc, char* argv[])
